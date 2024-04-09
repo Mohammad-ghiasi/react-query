a4ea0b77082849f90a88271456c8ab15822d2f53
mohammad <mohammadghiasi005@gmail.com>
2024-04-09 13:32:11 +0330

    react-query-confing

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,18 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient: QueryClient = new QueryClient()
+const queryClient: QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 20000,
+      gcTime: 20000,
+      // refetchInterval: 2000,
+      refetchOnReconnect: false,
+      retry: 5000
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
