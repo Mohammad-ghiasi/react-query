5f472d6209549eb6e7d0e95a0a22d49ce0d397f9
mohammad <mohammadghiasi005@gmail.com>
2024-04-10 15:07:13 +0330

    filterData

--- src/components/cardpage/Cardpage.tsx
+++ src/components/cardpage/Cardpage.tsx
@@ -1,14 +1,14 @@
-import axios from 'axios'
 import { useGet } from '../../hooks/useget'
 import { cardInfo } from '../../types/cardinfo'
 import Card from '../card/Card'
 
 const Cardpage = () => {
     const {data, isPending} = useGet("http://localhost:8000/ads/advertises/")
+
+
     if (isPending) {
         return <h1>Loading...</h1>
     }
-
     return (
         <>
             <h1>Cardpage components</h1>
--- src/hooks/useget.ts
+++ src/hooks/useget.ts
@@ -1,13 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosResponse } from "axios";
+import { cardInfo } from "../types/cardinfo";
 
-export const useGet = (url: string) => {
+type stat = null | "buy" | "sell";
+
+export const useGet = (url: string, type?: stat) => {
   const { data, isPending, error, isError } = useQuery({
-    queryKey: ["Posts"],
-    queryFn: async () =>
-      await axios.get(url).then((res: AxiosResponse) => {
+    queryKey: ["Posts", type],
+    queryFn: async (): Promise<any> => {
+      let addres = url;
+
+      if (type) {
+        addres = addres +  `?ad_type=${type}`
+      }
+      const datas: cardInfo[] = await axios.get(addres).then((res: AxiosResponse) => {
         return res.data;
-      }),
+      });
+      return datas;
+    },
   });
   return { data, isPending, error, isError };
 };
