18f67ea6cdad532150928296e3b18bcf42bb5fde
mohammad <mohammadghiasi005@gmail.com>
2024-04-11 17:53:02 +0330

    react-query-v1.1.2

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,15 @@
 import { cardInfo } from "../../types/cardinfo";
 
 const Card = (props: cardInfo) => {
-    const { id, ad_type, description, price, title } = props
+    const { id, ad_type, description,image, price, title } = props
     return (
         <>
-            <div>
-                <h1>{title}</h1>
-                {/* <img src={image} alt={title} /> */}
+            <div className="border border-black m-6 p-4">
+                <h1 className="text-lg font-bold">{title}</h1>
+                <div className="w-[100%] relative">
+                    <img src={image} alt={title} />
+                    <div className={`absolute font-bold top-3 left-4 ${ad_type === 'buy' ? 'bg-blue-500' : 'bg-red-500'} text-white px-4 py-1 rounded-2xl`}>{ad_type}</div>
+                </div>
                 <hr />
                 <h3>{ad_type}</h3>
                 <h4>{description}</h4>
